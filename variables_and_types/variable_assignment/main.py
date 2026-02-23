bf6bc6a38052acfed4ec40fcf079bfa445e56b16
kehicks-sudo <>
2026-02-23 14:03:12 +0000

    Variable Assignment

---
+++ variables_and_types/variable_assignment/main.py
@@ -0,0 +1,10 @@
+item_name = str("Eggs")
+item_price = float(3.99)
+item_stock_quantity = int(120)
+in_stock = True
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
