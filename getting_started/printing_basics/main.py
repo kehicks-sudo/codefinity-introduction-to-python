37a1be8eaaeb2b10a772e92a8e533ec560cfb158
kehicks-sudo <>
2026-02-25 15:01:25 +0000

    Printing Basics

--- getting_started/printing_basics/main.py
+++ getting_started/printing_basics/main.py
@@ -1,2 +1,9 @@
-sentence = "Hello from \"the little apple!\""
-print(sentence)
+a = 5
+b = 10
+c = 15
+x = b > a and c < b or (not (a + b > c))
+y = b + a >= c or c / a > b / a
+z = c - b != a and not c % a == 0
+print(x)
+print(y)
+print(z)
