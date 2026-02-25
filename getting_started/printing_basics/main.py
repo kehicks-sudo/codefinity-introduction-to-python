f5a8f922c3e60debbd0939351a3f795360494d2b
kehicks-sudo <>
2026-02-25 14:41:28 +0000

    Printing Basics

--- getting_started/printing_basics/main.py
+++ getting_started/printing_basics/main.py
@@ -1,7 +1,2 @@
-text_one = input("Enter the price of one item: ")
-price = float(text_one)
-text_two = input("Enter the quantity of items: ")
-quantity = int(text_two)
-cost = price * quantity
-print("The total cost is $", end="")
-print(cost)
+sentence = "Hello from \"the little apple!\""
+print(sentence)
