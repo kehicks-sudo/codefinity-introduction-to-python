fda31f0ff858ad44c5f66a89dac51f01f0888674
kehicks-sudo <>
2026-03-27 21:16:38 +0000

    Challenge: Pricing Adjustment Capstone

---
+++ other_data_types/challenge_pricing_adjustment_capstone/main.py
@@ -0,0 +1,10 @@
+# Task 1: Define a dictionary for grocery inventory
+grocery_inventory = {
+    "Milk": ("Dairy", 3.50, 8),
+    "Eggs": ("Dairy", 5.50, 30),
+    "Bread": ("Bakery", 2.99, 15),
+    "Apples": ("Produce", 1.50, 50),
+}
+
+# Task 2: Check Eggs price and reduce if needed
+grocery_inventory["Milk"][1]=2.5
